cf320cfc8d4deff3a346e55b9ff3ded7d9d9613f
Tdibacco17 <tomas.dibacco@25watts.com.ar>
2024-03-17 13:54:29 -0300

    fix

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,8 +1,7 @@
 import { revalidateTag } from "next/cache";
-import { NextResponse } from "next/server";
 
 export async function GET() {
     revalidateTag('cocina');
 
-    return NextResponse.json({ revalidate: true })
+    return Response.json({ revalidate: true })
 }
